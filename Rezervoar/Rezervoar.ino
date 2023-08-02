a8409656e7f15bdb9194b18c339acc5b32265410
tilenkov11 <tilen.kovacic1@gmail.com>
2023-08-02 12:33:38 +0200

    Update Rezervoar.ino
    Change to powering water level sonda only when reading to lower consumption

--- Rezervoar/Rezervoar.ino
+++ Rezervoar/Rezervoar.ino
@@ -4,7 +4,7 @@
 //#include <WiFi.h>
 //#include <SD.h>
 
-
+#define PROBE_POWER 14 // 3.3V to probe
 #define PROBE_PIN_1 4 // 25% probe
 #define PROBE_PIN_2 2 // 50% probe
 #define PROBE_PIN_3 15 // 75% probe
@@ -13,13 +13,14 @@
 #define LORA_SENDER 1
 
 void setup()
-{   
+{  
+    pinMode(PROBE_POWER, OUTPUT); 
     pinMode(PROBE_PIN_1, INPUT);
     pinMode(PROBE_PIN_2, INPUT);
     pinMode(PROBE_PIN_3, INPUT);
     pinMode(PROBE_PIN_4, INPUT);
     pinMode(BAT_V, INPUT);
-    Serial.begin(115200);
+    //Serial.begin(9600);
     while (!Serial);
 
  
@@ -27,7 +28,7 @@ void setup()
     SPI.begin(CONFIG_CLK, CONFIG_MISO, CONFIG_MOSI, CONFIG_NSS);
     LoRa.setPins(CONFIG_NSS, CONFIG_RST, CONFIG_DIO0);
     if (!LoRa.begin(BAND)) {
-        Serial.println("Starting LoRa failed!");
+       // Serial.println("Starting LoRa failed!");
         while (1);
     }
     if (!LORA_SENDER) {
@@ -40,6 +41,8 @@ void setup()
 
 void loop()
 {
+    digitalWrite(PROBE_POWER, HIGH); 
+    delay(1000);
     float BAT_VOL = (float)(analogRead(BAT_V)) * 3600 / 4095 * 2 / 1000 - 0.11;
     int probeState1 = digitalRead(PROBE_PIN_1);
     int probeState2 = digitalRead(PROBE_PIN_2);
@@ -49,6 +52,9 @@ void loop()
     int waterLevel2 = probeState2 == HIGH ? 100 : 0;
     int waterLevel3 = probeState3 == HIGH ? 100 : 0;
     int waterLevel4 = probeState4 == HIGH ? 100 : 0;
+    //Serial.println(waterLevel1);
+    digitalWrite(PROBE_POWER, LOW); 
+    delay(200);
 
 #if LORA_SENDER
     if (1 == 1) {
@@ -64,7 +70,8 @@ void loop()
         LoRa.endPacket();
     } 
     
-   ESP.deepSleep(3600e6);
-
 #endif
+
+ ESP.deepSleep(3600e6);
+
 }
